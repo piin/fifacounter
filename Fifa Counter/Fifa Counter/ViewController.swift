68c59e6f58dfe7a4734074407b4dc89a7e5b22b8
Jair Pinedo <jair.pin91@gmail.com>
2019-01-15 17:17:51 -0600

    initialize the project

---
+++ Fifa Counter/Fifa Counter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fifa Counter
+//
+//  Created by Jair Pinedo on 1/15/19.
+//  Copyright © 2019 Unosquare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
